d8dd1eb35dc65c4a9c45deb7d24635875340911b
aliyassine7 <aliyassine43@gmail.com>
2024-05-19 10:51:33 -0500

    first commit

---
+++ cypress/e2e/project02.cy.js
@@ -0,0 +1,45 @@
+describe('Login Automation Tests', () => {
+  beforeEach(() => {
+     
+  });
+
+  it('Test Case 01 - Validate the login form', () => {
+      
+  });
+
+  it('Test Case 02 - Validate the valid login', () => {
+      
+  });
+
+  it('Test Case 03 - Validate the logout', () => {
+     
+  });
+
+  it('Test Case 04 - Validate the Forgot Password? Link and Reset Password modal', () => {
+     
+  });
+
+  it('Test Case 05 - Validate the Reset Password modal close button', () => { 
+      
+  });
+
+  it('Test Case 06 - Validate the Reset Password form submission', () => {
+     
+  });
+
+  it('Test Case 07 - Validate the invalid login with empty credentials', () => {
+      
+  });
+
+  it('Test Case 08 - Validate the invalid login with the wrong username', () => {
+      
+  });
+
+  it('Test Case 09 - Validate the invalid login with the wrong password', () => {
+      
+  });
+
+  it('Test Case 10 - Validate the invalid login with the wrong username and password', () => {
+      
+  });
+});
